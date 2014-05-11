476eea2e88033452ad0196b229158f6ff9a76158
Kyle Mahan <kyle.mahan@gmail.com>
2014-05-11 11:07:48 -0700

    Handle (discard) microseconds as part of a datetime

--- mf2util/dt.py
+++ mf2util/dt.py
@@ -21,7 +21,7 @@ def parse(s):
 
     s = re.sub('\s+', ' ', s)
     date_re = "(?P<year>\d{4,})-(?P<month>\d{1,2})-(?P<day>\d{1,2})"
-    time_re = "(?P<hour>\d{1,2}):(?P<minute>\d{2})(:(?P<second>\d{2}))?"
+    time_re = "(?P<hour>\d{1,2}):(?P<minute>\d{2})(:(?P<second>\d{2})(\.(?P<microsecond>\d+))?)?"
     tz_re = "(?P<tzz>Z)|(?P<tzsign>[+-])(?P<tzhour>\d{1,2}):?(?P<tzminute>\d{2})"
     dt_re = "%s((T| )%s ?(%s)?)?$" % (date_re, time_re, tz_re)
 
